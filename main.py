e5847d98394439a5762449278551f461d6d830cd
ezh1k-mades-me-happy <kubickrubick58@gmail.com>
2024-02-18 20:33:35 +0300

    1th commit

--- main.py
+++ main.py
@@ -1,45 +1,33 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget
-from PyQt5.QtGui import QPainter, QColor
-from PyQt5.QtCore import Qt
-import random
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.uic import loadUi
+import sqlite3
 
 
-class MyWindow(QMainWindow):
+class CoffeeApp(QMainWindow):
     def __init__(self):
-        super(MyWindow, self).__init__()
+        super(CoffeeApp, self).__init__()
+        loadUi('main.ui', self)
+        self.setWindowTitle("Coffee Information")
 
-        self.setWindowTitle("Random Circles")
-        self.setGeometry(100, 100, 400, 300)
+        self.load_data()
 
-        layout = QVBoxLayout()
+    def load_data(self):
+        connection = sqlite3.connect('coffee.sqlite')
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM coffee")
+        data = cursor.fetchall()
+        connection.close()
 
-        self.button = QPushButton("Click me")
-        self.button.clicked.connect(self.on_button_click)
-
-        layout.addWidget(self.button)
-
-        self.widget = QWidget()
-        self.widget.setLayout(layout)
-        self.setCentralWidget(self.widget)
-
-    def on_button_click(self):
-        self.update()
-
-    def paintEvent(self, event):
-        painter = QPainter(self)
-        painter.setPen(Qt.NoPen)
-
-        color = QColor(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-        painter.setBrush(color)
-
-        diameter = random.randint(10, 100)
-        painter.drawEllipse(random.randint(0, self.width() - diameter), random.randint(0, self.height() - diameter),
-                            diameter, diameter)
+        for row in data:
+            self.tableWidget.insertRow(self.tableWidget.rowCount())
+            for col, value in enumerate(row):
+                item = QTableWidgetItem(str(value))
+                self.tableWidget.setItem(self.tableWidget.rowCount() - 1, col, item)
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    window = MyWindow()
+    window = CoffeeApp()
     window.show()
     sys.exit(app.exec_())
