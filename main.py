592aef5e1bffd9075dcc1b9d76ec6b6c10570183
ezh1k-mades-me-happy <kubickrubick58@gmail.com>
2024-02-18 20:20:10 +0300

    2th commit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget
-from PyQt5.uic import loadUi
 from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtCore import Qt
 import random
@@ -9,10 +8,20 @@ import random
 class MyWindow(QMainWindow):
     def __init__(self):
         super(MyWindow, self).__init__()
-        loadUi('UI.ui', self)
 
-        self.button = self.findChild(QPushButton, 'button')
+        self.setWindowTitle("Random Circles")
+        self.setGeometry(100, 100, 400, 300)
+
+        layout = QVBoxLayout()
+
+        self.button = QPushButton("Click me")
         self.button.clicked.connect(self.on_button_click)
+
+        layout.addWidget(self.button)
+
+        self.widget = QWidget()
+        self.widget.setLayout(layout)
+        self.setCentralWidget(self.widget)
 
     def on_button_click(self):
         self.update()
@@ -20,7 +29,10 @@ class MyWindow(QMainWindow):
     def paintEvent(self, event):
         painter = QPainter(self)
         painter.setPen(Qt.NoPen)
-        painter.setBrush(QColor(255, 255, 0))
+
+        color = QColor(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+        painter.setBrush(color)
+
         diameter = random.randint(10, 100)
         painter.drawEllipse(random.randint(0, self.width() - diameter), random.randint(0, self.height() - diameter),
                             diameter, diameter)
